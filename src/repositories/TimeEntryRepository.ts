a6d2c14440ebc0e134cd049e2f455ca30c70559e
jhuanassumpcao <jlassumpcao@inf.ufsm.br>
2025-01-09 14:42:46 -0300

    feat: create time entry repository

---
+++ src/repositories/TimeEntryRepository.ts
@@ -0,0 +1,25 @@
+import prisma from '../config/database';
+import { CreateTimeEntryDTO, UpdateTimeEntryDTO } from '../dtos/CreateTimeEntryDTO';
+
+export default class TimeEntryRepository {
+  static async create(data: CreateTimeEntryDTO) {
+    return prisma.timeEntry.create({ data });
+  }
+
+  static async findOngoingEntry(userId: number) {
+    return prisma.timeEntry.findFirst({
+      where: { userId, status: 'ongoing' },
+    });
+  }
+
+  static async update(id: number, data: Partial<UpdateTimeEntryDTO & { totalHours: number; status: string }>) {
+    return prisma.timeEntry.update({
+      where: { id },
+      data,
+    });
+  }
+
+  static async list() {
+    return prisma.timeEntry.findMany();
+  }
+}
